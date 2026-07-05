e1e13ef4ba30c1f4b5e9ffe81a8dae3a93bc32e7
Eftekhar30 <eftekhar.hossain30@gmail.com>
2026-07-06 01:51:33 +0600

    Allowed Vercel frontend in CORS policy

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ const app = express()
 
 connectDB()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://management-frontend-five.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 app.use(express.json())
 
 app.use('/api', indexRouter)
